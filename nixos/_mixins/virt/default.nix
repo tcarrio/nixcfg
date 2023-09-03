abf7761584adb531ca94e6fc286d3cf0efd9097a
Tom Carrio <tom@carrio.dev>
2023-09-03 01:44:47 -0400

    feat: use docker and fix screentearing

--- nixos/_mixins/virt/default.nix
+++ nixos/_mixins/virt/default.nix
@@ -1,28 +1,11 @@
-{ config, desktop, lib, pkgs, ... }: {
-  imports = [ ] ++ lib.optional (builtins.isString desktop) ./desktop.nix;
-
-  #https://nixos.wiki/wiki/Podman
-  environment.systemPackages = with pkgs; [
-    #buildah
-    #conmon
-    distrobox
-    #dive
-    fuse-overlayfs
-    #grype
-    podman-compose
-    podman-tui
-    #skopeo
-    #syft
-  ];
-
-  virtualisation = {
-    podman = {
-      defaultNetwork.settings = {
-        dns_enabled = true;
-      };
-      dockerCompat = true;
-      enable = true;
-      enableNvidia = lib.elem "nvidia" config.services.xserver.videoDrivers;
-    };
-  };
+{ config, containerization, desktop, lib, pkgs, ... }:
+{
+  imports =
+    [
+      ./docker.nix
+      # ./podman.nix
+    ]
+    ++ lib.optional (builtins.isString desktop) ./desktop.nix;
+    # TODO: Make containerization tools dynamic
+    # ++ lib.optional (builtins.isString containerization) ./${containerization}.nix;
 }
--- nixos/_mixins/virt/desktop.nix
+++ nixos/_mixins/virt/desktop.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   environment.systemPackages = with pkgs; [
     unstable.quickemu
     # for running X11 apps in distrobox
