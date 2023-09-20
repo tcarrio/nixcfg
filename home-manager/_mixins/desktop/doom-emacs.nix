8e9e30757fa955788717e5ff6755032d13dd19a8
Tom Carrio <tom@carrio.dev>
2023-09-20 19:30:23 -0400

    feat: initial version of doom-emacs and the provided config -- WIP

---
+++ home-manager/_mixins/desktop/doom-emacs.nix
@@ -0,0 +1,11 @@
+{ inputs, ... }: {
+    imports = [
+        inputs.nix-doom-emacs.hmModule
+    ];
+
+    programs.doom-emacs = {
+        enable = true;
+        doomPrivateDir = ./doom.d; # Directory containing your config.el, init.el
+                                    # and packages.el files
+    };
+}
