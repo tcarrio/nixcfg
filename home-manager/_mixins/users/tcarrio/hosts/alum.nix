70d119feb9d32897df22507fc1ce0c3f718dffd7
Tom Carrio <tom@carrio.dev>
2023-09-22 13:01:01 -0400

    feat: use PATH for skillshare-workstation and use recursive reference for sk/sw

--- home-manager/_mixins/users/tcarrio/hosts/alum.nix
+++ home-manager/_mixins/users/tcarrio/hosts/alum.nix
@@ -6,13 +6,21 @@
   ];
 
   home = {
-    sessionPath = [ "/opt/homebrew/bin" ];
+    sessionPath = [
+      "$HOME/Developer/workstation/bin"
+      "/opt/homebrew/bin"
+    ];
     sessionVariables = {
       SSH_AUTH_SOCK = "/Users/tcarrio/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh";
     };
     file = {
       "${config.xdg.configHome}/direnv/direnv.toml".text = builtins.readFile ./alum-direnv.toml;
     };
+  };
+
+  programs.fish.shellAliases = rec {
+    sk = "~/Developer/workstation/bin/skillshare-workstation";
+    sw = sk;
   };
 
   programs.git.userEmail = lib.mkForce "thomas.carrio@skillshare.com";
