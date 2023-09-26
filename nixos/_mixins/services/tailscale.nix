73c6074a4e4eb483bb5781ca39694c1e2f95c97f
Tom Carrio <tom@carrio.dev>
2023-09-26 00:10:11 -0400

    feat: separate autoconnection functionality from tailscale module

--- nixos/_mixins/services/tailscale.nix
+++ nixos/_mixins/services/tailscale.nix
@@ -1,12 +1,4 @@
 { config, pkgs, ... }: {
-  # Mounts the secrets file
-  age.secrets.tailscale-token = {
-    file = ../../../secrets/services/tailscale/token.age;
-    owner = "root";
-    group = "root";
-    mode = "600";
-  };
-
   environment.systemPackages = with pkgs; [ tailscale ];
 
   services.tailscale.enable = true;
@@ -17,32 +9,5 @@
       allowedUDPPorts = [ config.services.tailscale.port ];
       trustedInterfaces = [ "tailscale0" ];
     };
-  };
-
-  systemd.services.tailscale-autoconnect = {
-    description = "Automatic connection to Tailscale";
-
-    # make sure tailscale is running before trying to connect to tailscale
-    after = [ "network-pre.target" "tailscale.service" ];
-    wants = [ "network-pre.target" "tailscale.service" "run-agenix.d.mount" ];
-    wantedBy = [ "multi-user.target" ];
-
-    # set this service as a oneshot job
-    serviceConfig.Type = "oneshot";
-
-    # have the job run this shell script
-    script = with pkgs; ''
-      # wait for tailscaled to settle
-      sleep 2
-
-      # check if we are already authenticated to tailscale
-      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then # if so, then do nothing
-        exit 0
-      fi
-
-      # otherwise authenticate with tailscale
-      ${tailscale}/bin/tailscale up -authkey "$(cat "${config.age.secrets.tailscale-token.path}")"
-    '';
   };
 }
