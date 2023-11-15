0d182b2b4a11ed10a8d86eba97d080801451b076
Tom Carrio <tom@carrio.dev>
2023-11-15 18:14:42 -0500

    chore: remove helix

--- macos/_mixins/users/tcarrio/default.nix
+++ macos/_mixins/users/tcarrio/default.nix
@@ -2,7 +2,6 @@
   imports = [
     ../../nixos/console/auth0.nix
     ../../nixos/console/direnv.nix
-    ../../nixos/console/helix.nix
     ../../nixos/console/kubectl.nix
     ../../nixos/desktop/spotify.nix
   ];
