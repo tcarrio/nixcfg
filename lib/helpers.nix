aae1d750b01d1098c6e1e307eb2724fbc31ee422
Tom Carrio <tom@carrio.dev>
2023-11-27 20:36:25 -0500

    docs: add comments for various system types in helpers

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -50,11 +50,11 @@ in
   };
 
   forAllSystems = inputs.nixpkgs.lib.genAttrs [
-    "armv7l-linux"
-    "aarch64-linux"
-    "i686-linux"
-    "x86_64-linux"
-    "aarch64-darwin"
-    "x86_64-darwin"
+    "armv7l-linux"   # 32-bit ARM Linux
+    "aarch64-linux"  # 64-bit ARM Linux
+    "i686-linux"     # 32-bit x86 Linux
+    "x86_64-linux"   # 64-bit x86 Linux
+    "aarch64-darwin" # 64-bit ARM Darwin
+    "x86_64-darwin"  # 64-bit x86 Darwin
   ];
 }
