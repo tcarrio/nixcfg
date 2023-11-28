09a075346496cf86d90b144b375d664ee9aacbef
Tom Carrio <tom@carrio.dev>
2023-11-27 20:37:17 -0500

    feat: wip PXE server config

---
+++ nixos/_mixins/users/pxe/default.nix
@@ -0,0 +1,10 @@
+{ sshMatrix, ... }:
+{
+  users.users.pxe = {
+    # mkpasswd -m sha-512
+    hashedPassword = "$6$72XmnQnco5HkNtFh$AG9emNqa1Ig/ZHGOIVM07cInlf/FIfo5PvtsADFOfiEv8VQNWJTOtwDILG6GbmJ/APGubRWoilpio9FlWw5d30";
+    openssh.authorizedKeys.keys = [
+      sshMatrix.systems.glass.tcarrio
+    ];
+  };
+}
---
+++ nixos/server/pxe-server/disks.nix
@@ -0,0 +1,29 @@
+{ disks ? [ "/dev/sda" ], ... }: {
+  disko.devices = {
+    disk = {
+      sda = {
+        device = builtins.elemAt disks 0;
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              # for grub MBR
+              type = "EF02";
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
+
