dbc5f77cc979848ed35cf6bb80a6e641cad48cfc
Tom Carrio <tom@carrio.dev>
2023-12-27 21:16:54 -0500

    feat: custom hotkeys and workspaces for hyprland

--- home-manager/_mixins/desktop/hyprland.nix
+++ home-manager/_mixins/desktop/hyprland.nix
@@ -1,3 +1,28 @@
-_: {
-  wayland.windowManager.hyprland.enable = true;
+{ pkgs, ... }: {
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      "$mod" = "SUPER";
+      bind =
+        [
+          "$mod, F, exec, ${pkgs.rofi}/bin/rofi"
+        ]
+        ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+          builtins.concatLists (builtins.genList (
+              x: let
+                ws = let
+                  c = (x + 1) / 10;
+                in
+                  builtins.toString (x + 1 - (c * 10));
+              in [
+                "$mod, ${ws}, workspace, ${toString (x + 1)}"
+                "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+              ]
+            )
+            10)
+        );
+    };
+  }
 }
