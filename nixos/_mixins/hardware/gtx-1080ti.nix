134ec0da38912a201cc79b22e5d7ea74279bd130
Tom Carrio <tom@carrio.dev>
2023-12-30 21:01:46 -0500

    feat: consolidate nvidia and vulkan and remove vulkan module

--- nixos/_mixins/hardware/gtx-1080ti.nix
+++ nixos/_mixins/hardware/gtx-1080ti.nix
@@ -1,13 +1,46 @@
-{ pkgs, config, ... }: {
-  imports = [
-    ../av/vulkan.nix
+{ pkgs, config, ... }:
+let
+  vulkanDriverFiles = [
+    "${config.hardware.nvidia.package.bin}/share/vulkan/icd.d/nvidia_icd.x86_64.json"
+    "${config.hardware.nvidia.package.lib32}/share/vulkan/icd.d/nvidia_icd.i686.json"
   ];
+in
+{
+  environment = {
+    systemPackages = with pkgs; [ vulkan-tools nvtop ];
 
-  environment.systemPackages = with pkgs; [
-    nvtop
-  ];
+    variables = {
+      VK_DRIVER_FILES = (builtins.concatStringsSep ":" vulkanDriverFiles);
+    };
+  };
 
-  hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware = {
+    nvidia = {
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+      modesetting.enable = true;
+
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      powerManagement.enable = false;
+
+      # Only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
+
+      # Use the NVidia open source kernel module:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      open = false;
+
+      nvidiaSettings = true;
+    };
+
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+
+    pulseaudio.support32Bit = true;
   };
 }
