8e6be1855048da0f4ba55cc174f0010f357b579a
Tom Carrio <tom@carrio.dev>
2024-01-21 22:23:10 -0500

    chore: remove bat due to nix config issues

--- home-manager/_mixins/console/default.nix
+++ home-manager/_mixins/console/default.nix
@@ -118,13 +118,10 @@
     fish = {
       enable = true;
       shellAliases = {
-        cat = "bat --paging=never --style=plain";
         diff = "diffr";
         glow = "glow --pager";
         htop = "btm --basic --tree --hide_table_gap --dot_marker --mem_as_value";
         ip = "ip --color --brief";
-        less = "bat --paging=always";
-        more = "bat --paging=always";
         top = "btm --basic --tree --hide_table_gap --dot_marker --mem_as_value";
         tree = "eza --tree";
       };
