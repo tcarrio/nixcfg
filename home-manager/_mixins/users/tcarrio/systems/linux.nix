5b8fa9f9e148cb02e1eb15d9846f1b1e8692e95f
Tom Carrio <tom@carrio.dev>
2024-02-09 09:56:20 -0500

    fix: move quickemu references into linux-only system dir

--- home-manager/_mixins/users/tcarrio/systems/linux.nix
+++ home-manager/_mixins/users/tcarrio/systems/linux.nix
@@ -8,6 +8,30 @@
     ../../../desktop/emote.nix
   ];
 
+  home = {
+    file."Quickemu/nixos-console.conf".text = ''
+        #!/run/current-system/sw/bin/quickemu --vm
+        guest_os="linux"
+        disk_img="nixos-console/disk.qcow2"
+        disk_size="96G"
+        iso="nixos-console/nixos.iso"
+    '';
+    file."Quickemu/nixos-desktop.conf".text = ''
+        #!/run/current-system/sw/bin/quickemu --vm
+        guest_os="linux"
+        disk_img="nixos-desktop/disk.qcow2"
+        disk_size="96G"
+        iso="nixos-desktop/nixos.iso"
+    '';
+    file."Quickemu/nixos-nuc.conf".text = ''
+        #!/run/current-system/sw/bin/quickemu --vm
+        guest_os="linux"
+        disk_img="nixos-nuc/disk.qcow2"
+        disk_size="96G"
+        iso="nixos-nuc/nixos.iso"
+    '';
+  };
+
   systemd.user.tmpfiles.rules = [
     "d /home/${username}/0xc                           0755 ${username} users - -"
     "d /home/${username}/Code                          0755 ${username} users - -"
