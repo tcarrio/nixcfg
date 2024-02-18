8c95bd42ea47882a6d6c2b2651514abe073e880f
Tom Carrio <tom@carrio.dev>
2024-02-18 01:04:00 -0500

    fix: network manager wait service hanging in rebuild

--- nixos/workstation/t510/default.nix
+++ nixos/workstation/t510/default.nix
@@ -13,6 +13,8 @@
     inputs.nixos-hardware.nixosModules.common-pc
     inputs.nixos-hardware.nixosModules.common-pc-ssd
 
+    # fix for nixos-rebuild hangups on certain hardware
+    ../../_mixins/hardware/disable-nm-wait.nix
     ../../_mixins/hardware/grub-legacy-boot.nix
     ../../_mixins/services/pipewire.nix
     ../../_mixins/services/tailscale.nix
@@ -33,14 +35,20 @@
   services.nbd.server = {
     enable = true;
     listenAddress = "0.0.0.0";
+    listenPort = 10809;
+
+    extraOptions = {
+      allowlist = true;
+    };
 
     exports = {
       dvd-drive = {
         path = "/dev/sr0";
-        allowAddresses = [ "10.0.0.0/8" ];
+        allowAddresses = [ "192.168.1.0/24" "100.0.0.0/8" ];
       };
     };
   };
+  networking.firewall.allowedTCPPorts = [ 10809 ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
