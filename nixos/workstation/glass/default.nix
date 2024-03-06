dc0b464a26476322152e1c2b8d3a03c61b70f65b
Tom Carrio <tom@carrio.dev>
2024-03-05 22:38:54 -0500

    chore: disable nfs until functional from server-side

--- nixos/workstation/glass/default.nix
+++ nixos/workstation/glass/default.nix
@@ -22,7 +22,6 @@
     ../../_mixins/desktop/steam.nix
     ../../_mixins/hardware/gtx-1080ti.nix
     ../../_mixins/hardware/systemd-boot.nix
-    ../../_mixins/network-shares/ds418-nfs.nix
     ../../_mixins/services/pipewire.nix
     ../../_mixins/services/tailscale-autoconnect.nix
     ../../_mixins/virt
