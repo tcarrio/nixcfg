e22076f55cf398b110573b9a4db9fd01c0c44b3b
Tom Carrio <tom@carrio.dev>
2024-03-12 00:48:52 -0400

    feat: initial version of COSMIC desktop on NixOS
    NOT YET SUPPORTED

---
+++ nixos/_mixins/desktop/cosmic.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+{
+  imports = [
+    ../services/xdg-portal.nix
+  ];
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = false;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Enable udev rules
+  services.udev.packages = with pkgs.unstable; [ gnome.cosmic-settings-daemon ];
+
+  environment.systemPackages = with pkgs.unstable; [
+    gnomeExtensions.appindicator
+    gnome3.gnome-tweaks
+  ];
+}
