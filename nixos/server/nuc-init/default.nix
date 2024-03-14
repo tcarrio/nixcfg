392ecc3fce08d523a24fa1cd61c788fc31727d81
Tom Carrio <tom@carrio.dev>
2024-03-13 20:55:02 -0400

    format: uppercase for MAC addresses in nuc-init

--- nixos/server/nuc-init/default.nix
+++ nixos/server/nuc-init/default.nix
@@ -39,7 +39,7 @@ let
     end
 
     if [ -z "$hostname" ]
-      echo "No hostname determined. Auto-install will exit now.
+      echo "No hostname determined. auto-install-system will exit now.
       exit 1
     fi
 
@@ -79,16 +79,16 @@ in
   systemd.network = {
     enable = true;
     networks = {
-      "10-lan-200" = mkNetwork "f4:4d:30:61:9b:19" "200";
-      "10-lan-201" = mkNetwork "f4:4d:30:62:4c:26" "201";
-      "10-lan-202" = mkNetwork "f4:4d:30:61:99:ab" "202";
-      "10-lan-203" = mkNetwork "f4:4d:30:61:8c:cf" "203";
-      "10-lan-204" = mkNetwork "f4:4d:30:61:99:ad" "204";
-      "10-lan-205" = mkNetwork "f4:4d:30:61:8a:9d" "205";
-      "10-lan-206" = mkNetwork "f4:4d:30:62:4a:76" "206";
-      "10-lan-207" = mkNetwork "f4:4d:30:62:4a:43" "207";
-      "10-lan-208" = mkNetwork "f4:4d:30:61:9a:e0" "208";
-      "10-lan-209" = mkNetwork "f4:4d:30:61:99:ed" "209";
+      "10-lan-200" = mkNetwork "F4:4D:30:61:9B:19" "200";
+      "10-lan-201" = mkNetwork "F4:4D:30:62:4C:26" "201";
+      "10-lan-202" = mkNetwork "F4:4D:30:61:99:AB" "202";
+      "10-lan-203" = mkNetwork "F4:4D:30:61:8C:CF" "203";
+      "10-lan-204" = mkNetwork "F4:4D:30:61:99:AD" "204";
+      "10-lan-205" = mkNetwork "F4:4D:30:61:8A:9D" "205";
+      "10-lan-206" = mkNetwork "F4:4D:30:62:4A:76" "206";
+      "10-lan-207" = mkNetwork "F4:4D:30:62:4A:43" "207";
+      "10-lan-208" = mkNetwork "F4:4D:30:61:9A:E0" "208";
+      "10-lan-209" = mkNetwork "F4:4D:30:61:99:ED" "209";
     };
   };
 
