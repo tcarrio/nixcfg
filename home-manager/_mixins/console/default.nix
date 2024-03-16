c2898bae4ca13c86cce8f5915cffa5ddb172c437
Tom Carrio <tom@carrio.dev>
2024-03-15 23:46:03 -0400

    chore: disable atuin

--- home-manager/_mixins/console/default.nix
+++ home-manager/_mixins/console/default.nix
@@ -56,22 +56,22 @@
 
   programs = {
     atuin = {
-      enable = true;
-      enableBashIntegration = true;
-      enableFishIntegration = true;
-      flags = [
-        "--disable-up-arrow"
-      ];
-      package = pkgs.unstable.atuin;
-      settings = {
-        auto_sync = true;
-        dialect = "us";
-        show_preview = true;
-        style = "compact";
-        sync_frequency = "1h";
-        sync_address = "https://api.atuin.sh";
-        update_check = false;
-      };
+      enable = false;
+      # enableBashIntegration = true;
+      # enableFishIntegration = true;
+      # flags = [
+      #   "--disable-up-arrow"
+      # ];
+      # package = pkgs.unstable.atuin;
+      # settings = {
+      #   auto_sync = true;
+      #   dialect = "us";
+      #   show_preview = true;
+      #   style = "compact";
+      #   sync_frequency = "1h";
+      #   sync_address = "https://api.atuin.sh";
+      #   update_check = false;
+      # };
     };
     bottom = {
       enable = true;
