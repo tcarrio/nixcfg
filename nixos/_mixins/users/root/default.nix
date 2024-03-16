f3e1a097a512af72ca191da47b6c90ea3b60d5a6
Tom Carrio <tom@carrio.dev>
2024-03-15 22:17:38 -0400

    fix: syntax for with in root user

--- nixos/_mixins/users/root/default.nix
+++ nixos/_mixins/users/root/default.nix
@@ -3,8 +3,9 @@
   users.users.root = {
     # mkpasswd -m sha-512
     hashedPassword = "$6$FGMdV6JzcaHdCnQt$yOu9i9B2NOxsb6MPg1yxgNOifyMC/QveHsADtTuTvxpahf0yb610y.fCkQolYgdAp4Ih1zHsRQS9U71yh5.iS1";
-    openssh.authorizedKeys.keys = with sshMatrix.systems.glass [
+    openssh.authorizedKeys.keys = with sshMatrix.systems.glass; [
       tcarrio
+      host
     ];
   };
 
