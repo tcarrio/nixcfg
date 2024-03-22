10cc947451c3eb1a245ef4641e632daf3034c9ee
Tom Carrio <tom@carrio.dev>
2024-03-22 13:53:44 -0400

    feat: enable idempotent operations in Brew during darwin-rebuild and brew commands
    explicit update / upgrade actions required through brew CLI

--- darwin/_mixins/console/homebrew.nix
+++ darwin/_mixins/console/homebrew.nix
@@ -3,5 +3,11 @@
     enable = true;
 
     taps = [];
+
+    # the following sets up Homebrew to NEVER update implicitly
+    # to update brew itself, use `brew upgrade`
+    # to update brew packages, use `brew update`
+    global.autoUpdate = false;
+    onActivation.autoUpdate = false;
   };
 }
