9c866a2dc1b8e68d3bdac9a8051496128a24a650
Tom Carrio <tom@carrio.dev>
2024-03-23 22:27:36 -0400

    feat: support for intel accelerated video playback

---
+++ nixos/_mixins/hardware/intel.accelerated-video-playback.nix
@@ -0,0 +1,16 @@
+_: {
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-compute-runtime
+    ];
+  };
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
+}
