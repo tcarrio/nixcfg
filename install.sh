259bf715d698d41a1e6104fa66786a62c96d1625
Tom Carrio <tom@carrio.dev>
2024-07-09 18:53:35 -0400

    fix: no empty single-quotes in targets

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 #set -euo pipefail
 
-TARGET_HOST="''${1:-}"
-TARGET_USER="''${2:-tcarrio}"
-TARGET_TYPE="''${3:-}"
+TARGET_HOST="${1:-}"
+TARGET_USER="${2:-tcarrio}"
+TARGET_TYPE="${3:-}"
 
 if [ "$(id -u)" -eq 0 ]; then
   echo "ERROR! $(basename "$0") should be run as a regular user"
