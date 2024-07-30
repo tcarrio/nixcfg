312179ddc117194536ff41d223e1fc359c133246
Tom Carrio <tom@carrio.dev>
2024-07-30 16:39:58 -0400

    feat: initial Darwin module for desktop application support

--- darwin/_mixins/desktop/default.nix
+++ darwin/_mixins/desktop/default.nix
@@ -1,12 +1,21 @@
-{ config, pkgs, ... }: {
+{ lib, config, pkgs, ... }: {
+  options.oxc.desktop.applications = {
+    enable = lib.mkOption {
+      default = true;
+      description = "Whether to enable desktop application support in Darwin";
+    };
+  };
+
   # https://github.com/LnL7/nix-darwin/issues/214#issuecomment-2050027696
-  system.activationScripts.postUserActivation.text = ''
-    rsyncArgs="--archive --checksum --chmod=-w --copy-unsafe-links --delete"
-    apps_source="${config.system.build.applications}/Applications"
-    moniker="Nix Trampolines"
-    app_target_base="$HOME/Applications"
-    app_target="$app_target_base/$moniker"
-    mkdir -p "$app_target"
-    ${pkgs.rsync}/bin/rsync $rsyncArgs "$apps_source/" "$app_target"
-  '';
+  config = lib.mkIf config.oxc.desktop.applications.enable {
+    system.activationScripts.postUserActivation.text = ''
+      rsyncArgs="--archive --checksum --chmod=-w --copy-unsafe-links --delete"
+      apps_source="${config.system.build.applications}/Applications"
+      moniker="Nix Trampolines"
+      app_target_base="$HOME/Applications"
+      app_target="$app_target_base/$moniker"
+      mkdir -p "$app_target"
+      ${pkgs.rsync}/bin/rsync $rsyncArgs "$apps_source/" "$app_target"
+    '';
+  };
 }
