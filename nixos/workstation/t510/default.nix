a75d98d45acf1089af27487d81c8e912f392bf75
Tom Carrio <tom@carrio.dev>
2024-10-20 22:07:35 -0400

    refactor: use base iso-console config for headless base

--- nixos/workstation/t510/default.nix
+++ nixos/workstation/t510/default.nix
@@ -1,11 +1,19 @@
 # Device:      Lenovo ThinkPad T510
 # CPU:         Intel i5 M 520
 # RAM:         8GB DDR2
-# SATA:        120GB SSD
+# SATA:        500GB SSD
 
 { inputs, lib, pkgs, ... }: {
   imports = [
-    ../t510-headless
+    (import ./disks.nix { })
+
+    inputs.nixos-hardware.nixosModules.common-cpu-intel
+    inputs.nixos-hardware.nixosModules.common-pc
+    inputs.nixos-hardware.nixosModules.common-pc-ssd
+
+    # fix for nixos-rebuild hangups on certain hardware
+    ../../mixins/hardware/grub-legacy-boot.nix
+    ../../mixins/services/pipewire.nix
   ];
 
   environment.systemPackages = with pkgs; [
@@ -27,5 +35,43 @@
       chromium.enable = lib.mkForce false;
       firefox.enable = lib.mkForce false;
     };
+    services = {
+      tailscale.enable = true;
+    };
+    containerisation = {
+      enable = true;
+      engine = "podman";
+    };
+    virtualisation.enable = true;
   };
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    initrd.availableKernelModules = [ "ehci_pci" "ahci" "firewire_ohci" "usb_storage" "sd_mod" "sr_mod" "sdhci_pci" ];
+    initrd.kernelModules = [ ];
+    kernelModules = [ ];
+    extraModulePackages = [ ];
+  };
+
+  services.nbd.server = {
+    enable = false;
+    listenAddress = "0.0.0.0";
+    listenPort = 10809;
+
+    extraOptions = {
+      allowlist = true;
+    };
+
+    exports = {
+      dvd-drive = {
+        path = "/dev/sr0";
+        allowAddresses = [ "192.168.40.0/24" "100.0.0.0/8" ];
+      };
+    };
+  };
+  networking.firewall.allowedTCPPorts = [ 10809 ];
+  networking.networkmanager.enable = true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
+
