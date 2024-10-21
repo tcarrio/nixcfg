4488944f2d508720fa45bea3123a81f0a2a54204
Tom Carrio <tom@carrio.dev>
2024-10-20 22:28:16 -0400

    revert: simpler mbr with ext4 disko config for t510

--- nixos/workstation/t510-headless/disks.nix
+++ nixos/workstation/t510-headless/disks.nix
@@ -1,40 +1,23 @@
 { disks ? [ "/dev/sda" ], ... }: {
   disko.devices = {
     disk = {
-      main = {
+      sda = {
         device = "/dev/disk/by-id/ata-Samsung_SSD_850_PRO_512GB_S250NXAH211618D";
-        name = "Samsung 500GB 2.5\" SATA";
         type = "disk";
         content = {
           type = "gpt";
           partitions = {
-            # compatibility for BIOS 
             boot = {
               size = "1M";
+              # for grub MBR
               type = "EF02";
             };
             root = {
-              name = "root";
-              end = "-0";
+              size = "100%";
               content = {
                 type = "filesystem";
-                format = "f2fs";
+                format = "ext4";
                 mountpoint = "/";
-                extraArgs = [
-                  "-O"
-                  "extra_attr,inode_checksum,sb_checksum,compression"
-                ];
-                mountOptions = [
-                  "compress_algorithm=zstd:6,compress_chksum,atgc,gc_merge,lazytime,nodiscard"
-                ];
-              };
-            };
-            swap = {
-              size = "16G";
-              content = {
-                type = "swap";
-                discardPolicy = "both";
-                resumeDevice = true; # resume from hiberation from this device
               };
             };
           };
