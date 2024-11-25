3b80cef0d8f6268a000aadd884eeeeabec640edf
Tom Carrio <tom@carrio.dev>
2024-11-25 01:05:36 -0500

    chore: decomission references to i686-linux and armv7l-linux

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -95,9 +95,10 @@ in
   };
 
   forAllLinux = inputs.nixpkgs.lib.genAttrs [
-    "armv7l-linux" # 32-bit ARM Linux
+    ## So long and thanks for all the fish
+    # "armv7l-linux" # 32-bit ARM Linux
+    # "i686-linux" # 32-bit x86 Linux
     "aarch64-linux" # 64-bit ARM Linux
-    "i686-linux" # 32-bit x86 Linux
     "x86_64-linux" # 64-bit x86 Linux
   ];
 
