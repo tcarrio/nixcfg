6b4558813c4b9a3df699cc3921e48615990ec1c4
Tom Carrio <tom@carrio.dev>
2024-12-16 23:10:07 -0500

    feat: enable swraid in iso-console

--- nixos/iso/iso-console/default.nix
+++ nixos/iso/iso-console/default.nix
@@ -3,4 +3,7 @@
   console.keyMap = lib.mkForce "us";
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  # Some devices have RAID controllers that I want to utilize
+  boot.swraid.enable = true;
 }
