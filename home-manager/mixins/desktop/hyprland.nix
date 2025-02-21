a75c3b87387c108ac36d9b295686c6dbaeeed000
Tom Carrio <tom@carrio.dev>
2025-02-20 19:14:25 -0500

    feat: updates, themes, icons, etc. for Hyprland config

--- home-manager/mixins/desktop/hyprland.nix
+++ home-manager/mixins/desktop/hyprland.nix
@@ -28,5 +28,53 @@
             10)
         );
     };
+
+    plugins = [
+      inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprbars
+    ];
+
+    systemd.variables = ["--all"];
   };
+
+  environment.systemPackages = with pkgs; [
+    flat-remix-icon-theme
+    gnome-themes-extra
+    noto-fonts
+  ];
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+  };
+
+  gtk = {
+    enable = true;
+
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
+
+    iconTheme = {
+      package = pkgs.gnome.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+
+    font = {
+      name = "Noto Sans";
+      size = 11;
+    };
+  };
+
+  ### TODO: Remove? Redundant to `gtk` configs
+  # dconf.settings."org/gnome/desktop/interface" = {
+  #   gtk-theme = "Adwaita";
+  #   icon-theme = "Flat-Remix-GTK-Grey-Darkest";
+  #   document-font-name = "Noto Sans Medium 11";
+  #   font-name = "Noto Sans Medium 11";
+  #   monospace-font-name = "Noto Sans Mono Medium 11";
+  # };
 }
