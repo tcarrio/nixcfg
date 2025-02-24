3a6ee84ea9bd8ecc964f4d3dba08725894f09de5
Tom Carrio <tom@carrio.dev>
2025-02-24 00:16:41 -0500

    chore: rekey all secrets for new admin devices

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,32 +1,34 @@
 let
   sshMatrix = import ../lib/ssh-matrix.nix { };
   inherit (sshMatrix) systems;
-  inherit (systems) glass;
+  inherit (systems) glass obsidian t510;
 
   autoMeshSystems = [
-    glass.tcarrio
     glass.host
     systems.nix-proxy-droplet.host
   ];
+
+  admins = [ glass.tcarrio obsidian.tcarrio t510.tcarrio ];
 in
 {
-  "users/tcarrio/ssh.age".publicKeys = [ glass.tcarrio glass.host ];
-  "services/netbird/token.age".publicKeys = autoMeshSystems;
-  "services/tailscale/token.age".publicKeys = autoMeshSystems;
+  "users/tcarrio/ssh.age".publicKeys = admins ++ [ glass.tcarrio glass.host ];
+  "services/netbird/token.age".publicKeys = admins ++ autoMeshSystems;
+  "services/tailscale/token.age".publicKeys = admins ++ autoMeshSystems;
   # "services/jira-cli/token.age".publicKeys = macos;
-  "services/acme/cloudflare.age".publicKeys = with glass; [ tcarrio host systems.nix-proxy-droplet.host ];
+  "services/acme/cloudflare.age".publicKeys = admins ++ [ systems.nix-proxy-droplet.host ];
+  "services/hoarder/env.age".publicKeys = admins ++ [ systems.obsidian.host ];
 
-  "network-shares/ds418/smb.conf.age".publicKeys = [ glass.tcarrio ];
+  "network-shares/ds418/smb.conf.age".publicKeys = admins;
 
   # primarily maintained via agenix for convenience of scripting automations
-  "hosts/nuc0/ssh_host_ed25519_key.age".publicKeys = [ glass.tcarrio ];
-  "hosts/nuc1/ssh_host_ed25519_key.age".publicKeys = [ glass.tcarrio ];
-  "hosts/nuc2/ssh_host_ed25519_key.age".publicKeys = [ glass.tcarrio ];
-  "hosts/nuc3/ssh_host_ed25519_key.age".publicKeys = [ glass.tcarrio ];
-  "hosts/nuc4/ssh_host_ed25519_key.age".publicKeys = [ glass.tcarrio ];
-  "hosts/nuc5/ssh_host_ed25519_key.age".publicKeys = [ glass.tcarrio ];
-  "hosts/nuc6/ssh_host_ed25519_key.age".publicKeys = [ glass.tcarrio ];
-  "hosts/nuc7/ssh_host_ed25519_key.age".publicKeys = [ glass.tcarrio ];
-  "hosts/nuc8/ssh_host_ed25519_key.age".publicKeys = [ glass.tcarrio ];
-  "hosts/nuc9/ssh_host_ed25519_key.age".publicKeys = [ glass.tcarrio ];
+  "hosts/nuc0/ssh_host_ed25519_key.age".publicKeys = admins;
+  "hosts/nuc1/ssh_host_ed25519_key.age".publicKeys = admins;
+  "hosts/nuc2/ssh_host_ed25519_key.age".publicKeys = admins;
+  "hosts/nuc3/ssh_host_ed25519_key.age".publicKeys = admins;
+  "hosts/nuc4/ssh_host_ed25519_key.age".publicKeys = admins;
+  "hosts/nuc5/ssh_host_ed25519_key.age".publicKeys = admins;
+  "hosts/nuc6/ssh_host_ed25519_key.age".publicKeys = admins;
+  "hosts/nuc7/ssh_host_ed25519_key.age".publicKeys = admins;
+  "hosts/nuc8/ssh_host_ed25519_key.age".publicKeys = admins;
+  "hosts/nuc9/ssh_host_ed25519_key.age".publicKeys = admins;
 }
