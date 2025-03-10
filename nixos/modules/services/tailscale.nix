60b3c6f3205c0d6c238127b0c7bba79457747fff
Tom Carrio <tom@carrio.dev>
2025-03-09 22:25:17 -0400

    feat: additional module configs for Tailscale service

--- nixos/modules/services/tailscale.nix
+++ nixos/modules/services/tailscale.nix
@@ -1,18 +1,42 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }: let
+  cfg = config.oxc.services.tailscale;
+in
+{
   options.oxc.services.tailscale = {
     enable = lib.mkOption {
       type = lib.types.bool;
       default = false;
       description = "Enable the Tailscale mesh network service";
     };
+
+    ssh = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable the Tailscale SSH override for Tailnet hosts";
+      };
+    };
+
+    exitNode = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Allow the Tailscale service to serve as an exit node";
+      };
+    };
   };
 
-  config = lib.mkIf config.oxc.services.tailscale.enable {
+  config = lib.mkIf cfg.enable {
     # tailscale CLI
     environment.systemPackages = [ pkgs.tailscale ];
 
     # tailscale service
-    services.tailscale.enable = true;
+
+    services.tailscale = {
+      enable = true;
+      extraSetFlags = [] ++ (lib.optionals cfg.exitNode.enable ["--advertise-exit-node"]);
+      extraUpFlags = [] ++ (lib.optionals cfg.ssh.enable ["--ssh"]);
+    };
 
     # firewall integration
     networking = {
