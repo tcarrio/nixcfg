54f7dfdc839cdf4cdbb7c388392201ce81db3560
Tom Carrio <tom@carrio.dev>
2025-03-29 12:20:30 -0400

    fix: link to ASCII generator font

--- nixos/modules/services/iso-auto-installer.nix
+++ nixos/modules/services/iso-auto-installer.nix
@@ -1,7 +1,7 @@
 { lib, config, pkgs, ... }: {
   isoImage.isoName = lib.mkForce "0xc-auto-installer-${config.system.nixos.label}-${pkgs.stdenv.hostPlatform.system}.iso";
   # for the record, the ascii text art is ANSI Shadow
-  # https://patorjk.com/software/taag/#p=display&f=ANSI Shadow
+  # https://patorjk.com/software/taag/#p=display&f=ANSI+Shadow
   services.getty.helpLine = ''
      ██████╗ ██╗  ██╗ ██████╗    ██╗███╗   ██╗██╗██╗  ██╗ ██████╗███████╗ ██████╗ 
     ██╔═████╗╚██╗██╔╝██╔════╝   ██╔╝████╗  ██║██║╚██╗██╔╝██╔════╝██╔════╝██╔════╝ 
