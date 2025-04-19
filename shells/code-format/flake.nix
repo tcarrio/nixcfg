21088c943f00e2958dfd1edc51e12801f889c12f
Tom Carrio <tom@carrio.dev>
2025-04-19 18:49:22 -0400

    chore: update shells to 24.11

--- shells/code-format/flake.nix
+++ shells/code-format/flake.nix
@@ -2,7 +2,7 @@
   description = "Nix shell for code-format tools";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
