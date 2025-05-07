cbb7fe0cc7a801ea44d39cdc2838de62df5df27f
Tom Carrio <tom@carrio.dev>
2025-05-06 20:06:17 -0400

    chore: deprecate intel.accelerated-video-playback module and defer to nixos-hardware

--- nixos/mixins/hardware/intel.accelerated-video-playback.nix
+++ nixos/mixins/hardware/intel.accelerated-video-playback.nix
@@ -1,16 +1,9 @@
-_: {
-  nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  };
-  hardware.opengl = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-      vaapiIntel
-      vaapiVdpau
-      libvdpau-va-gl
-      intel-compute-runtime
-    ];
-  };
-  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
+# Legacy module. This defers entirely to nixos-hardware
+# See https://github.com/NixOS/nixos-hardware
+
+{ pkgs, inputs, ... }: {
+  imports = [
+    inputs.nixos-hardware.nixosModules.common-cpu-intel
+  ];
 }
+
