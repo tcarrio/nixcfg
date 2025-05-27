4435fc60c49d6564fd857261ecc8bf7771472248
Tom Carrio <tom@carrio.dev>
2025-05-26 23:55:07 -0400

    fix: definitions for i18n locales

--- nixos/default.nix
+++ nixos/default.nix
@@ -43,17 +43,17 @@ in
   };
 
   i18n = {
-    defaultLocale = "en_US.utf8";
+    defaultLocale = "en_US.UTF-8";
     extraLocaleSettings = {
-      LC_ADDRESS = "en_US.utf8";
-      LC_IDENTIFICATION = "en_US.utf8";
-      LC_MEASUREMENT = "en_US.utf8";
-      LC_MONETARY = "en_US.utf8";
-      LC_NAME = "en_US.utf8";
-      LC_NUMERIC = "en_US.utf8";
-      LC_PAPER = "en_US.utf8";
-      LC_TELEPHONE = "en_US.utf8";
-      LC_TIME = "en_US.utf8";
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
     };
   };
   services.xserver.xkb.layout = "us";
