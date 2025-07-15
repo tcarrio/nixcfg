40ae179e5b3d8c0afc5462699ccf23af088ab0e8
Tom Carrio <tom@carrio.dev>
2025-07-14 22:32:37 -0400

    feat: rework agenix groups and add spotify secret

--- lib/ssh-matrix.nix
+++ lib/ssh-matrix.nix
@@ -4,15 +4,22 @@ rec {
   systems = {
     sktc0.tcarrio = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBADpxbxYDu9fhk30IeUP21WUGn4nyAX0uff4o7fF+memMmrR4A4/V8H7vj2ucNunk4gbisvit1LQXEcINZVy+kk= GitHub-NoPrompt@secretive.alum.local";
 
+    # Glass RIP see obsidian
     glass.host = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDC1bp1dPv/g2DkghZprPoxqtxcqSZx5D0AG0rkR7BIDhYfrHGSLsre/RoL38epPpaitKWXr6C/Cbqgby9K/zLgBG+BGHKVmYGrvmQU+NlUjlhKrotaVFhJ5Q8RYDnX+8dZcjeKs5sCRGnU76AfoXm4RCggesJB4QThragtgEOrmaEUzTZJ4uRKYACNYNiYtggYMIk6X9E0fK0Jxh+YW9zvIwvzWc+SrjlCC2YqHSN+OKvco/6yLn+deT0SqIl027f181DtsgogsVAGXVzFVJ/1REXxuonEhjjoqn7n7A9Yu3mIc8jLa8XZaW3bIjb0R15PG35Ou9pCwfhFp1wOePDUojrv+JHaQB9pfiRK7ij6A6lGArBiKMIpL76RSRXNuATkTb9fXm24we1kt1uQ87dWze/mu6YakAlG9zESCYrp6l5YF2hCo7Nd9M9UFG/fjSQWvyOpUshgocqOUsttpluBs+R0h8T9+pvv2czORlO7WxeQrF5rEFNYwh/l5KwxA9VppTsZu29xXPYGb3pE+FeRFAMqy+l0lyJ9PzthIUlJgLRxH4xg23X8BpOXSA40O2HnIISbGgzDEgqzYZ79y+TrYeEICD+0NWIuosD1iJ7m8RkuC6NsU4bo9nHBBtIr69Ia6yt4IHz8b5jgdXw2rzfqYYIRkIicgDqpu0vSBkL7aQ== root@glass";
     glass.tcarrio = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDe7xvr45OB0rHkLy9EWSxGGe/OpmMqawr90+d/RXTP2F9H/X7DDByLuH1VMNtjYAltnRO4zQW6vod90EptfuiTOl6B1lipg2abkRg8yFe+BWSzgNdtzmaKp0UEA6+EbeQft/zGEaPcibcJT8OFEeGdYeMthq5bXiFowgBgdIaMxNo5wn7SButgvT6Jo9VXyWUbMG/HkIKpNzNP1bfMhYcrGGWVfiu3Lyvulj2lJ4OkbEXMQN+sPE+Ti5rUX07zc0sqAvohRxTwAeSJM7RndD4+rEb/tj8bpHYbvcZwmvmVculh/5+vDnWRXJCjbuj6gMnvn2Njbz7Mu6xRZx+ev4SdXpO5vdvd2BBV08fvrWx3iMAqGNppqS0h4UadtApWkx1TjMMuvyk+SZmAtv8OQ662SauS3pIUpr+o8D7BjS8my1mQP5MMW7M3rvpBJ5TlKf2RecDR43R1MZdKzgtKmTLtlarna30Xt4phDXhpyW7C5L9DruZ9MV1Gc4820juIplmN2/WGcwvLGxu+YMiN7jm26T/2bogr039Bpcb3aPLTTm1dkC4qowcFQ7Eww3QH5ECRg8e8b/BaQgA0CUXOZxDSIH3X3c0LdEUCuXVrCc9bHK8U8XFijbMkosIQabudqPiXBXDsrKDSu0qI//T315EfNeXGCocmM5Ujb2P7a2zZFQ== tom@carrio.dev";
 
+    # Obsidian desktop
     obsidian.host = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCjGeJn0nLBrJMLR3RNOiIotDURUw5NDPL23UtnnltaHYyInudreuJUdht1wiSu1CZeuyko0oawS+0w9kYz9OPJMobaYfnXrCMqsvJ8PB7B1Ddo4JPA2OqRCeWB4n+3Ez8n7RXDeEx0ms3xerotsGMFR+g2cQ5tw8si9co/PmrLJu+VRKUSEgaZN18D2LZlAG/+6uu/qXGWbIkM8Nodv4ZiBhGvaXLTWwWCHTIm1xW15009UIkVSituVvrACBQXjoM6CeBWfH9NbrZPogMWVpiF4kgvnAHTAu0Oe3k62vmyTlB71p2srXif4zJRWVdpqgjO3YLDLtgwrrakNe7twzhWoRmviKU1ZZyxZm7NKw917ZzsTaE2ZHZ5JRFFAC/t9SrGcT/PoeYMgUk6vWvJc3igX5WHcEMzIaBLhemKaPo3XpskWhg4bSYwig54CZ99uYhFINb8AhYTU6+//205jVB0JCTSIcKIsI5Jbq72pt3D7+9yeaDXR/dKQbf5CBCStGwFw8RvInDUpf4Qvyj4mnUZKpICXghVuR8swdNX7mXEJVSIkohEuu2iWnebvnx1bbZJqvHQB8gCQY4Ss8fUuiSEGYMoHQEh4LpHo8n2xTr9SrbJFYhjto+E/6npAf+P7cR1Y5QeMJYGvEfJMcLoUcCxTyLULMu6IfrAaGNo9+S2Qw== root@obsidian";
     obsidian.tcarrio = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDe7xvr45OB0rHkLy9EWSxGGe/OpmMqawr90+d/RXTP2F9H/X7DDByLuH1VMNtjYAltnRO4zQW6vod90EptfuiTOl6B1lipg2abkRg8yFe+BWSzgNdtzmaKp0UEA6+EbeQft/zGEaPcibcJT8OFEeGdYeMthq5bXiFowgBgdIaMxNo5wn7SButgvT6Jo9VXyWUbMG/HkIKpNzNP1bfMhYcrGGWVfiu3Lyvulj2lJ4OkbEXMQN+sPE+Ti5rUX07zc0sqAvohRxTwAeSJM7RndD4+rEb/tj8bpHYbvcZwmvmVculh/5+vDnWRXJCjbuj6gMnvn2Njbz7Mu6xRZx+ev4SdXpO5vdvd2BBV08fvrWx3iMAqGNppqS0h4UadtApWkx1TjMMuvyk+SZmAtv8OQ662SauS3pIUpr+o8D7BjS8my1mQP5MMW7M3rvpBJ5TlKf2RecDR43R1MZdKzgtKmTLtlarna30Xt4phDXhpyW7C5L9DruZ9MV1Gc4820juIplmN2/WGcwvLGxu+YMiN7jm26T/2bogr039Bpcb3aPLTTm1dkC4qowcFQ7Eww3QH5ECRg8e8b/BaQgA0CUXOZxDSIH3X3c0LdEUCuXVrCc9bHK8U8XFijbMkosIQabudqPiXBXDsrKDSu0qI//T315EfNeXGCocmM5Ujb2P7a2zZFQ== tom@carrio.dev";
 
+    # T510 RIP see void
     t510.tcarrio = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG/YX263nli8x0brqKtktwAaqmChPVyYcPqCKY5RDzZl tcarrio@t510";
     t510.host = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC9TxciSqBZXPjnHYlv1+2D1tjKYR2jLUigtANtdS34YESWveh0eiaYp7hHVi2BedVJOe/vKI6JllnX/CeNGtMI+PowVBniov5WvPkOTbS9p8fsGJP1OqPxlFR6jq7te6/KzLDbMMk91miFOTx27qZ2kWk01K7AGqNQ8BotBRojsAONB9M1JAv42CSuZmB/nKe2zWCpWoMhMGzqOD/TzrO3qLZ1rLUHkXV8cIWoDaWRngAV/sLMrrQglRvXz9GkNPq98JsXM0XL0GrEnEUz8rPx+kS7hF3Ysq2MNawd0CeaG8aeCoNwGep74nPT+V/nAs9xYa9M53fFAOO/Hoi0yxmZIe1324ure7QKgNLDiRIxPfIvf1LNvdNoxWjKKyaoTwRLh8E2Nn5dx94kzRfPaeMK6uMSNhoLu2blqI0omTmKi9OKgmmZpB1+2vK7C24HJQ2xQU4UR63SlwvTT+u38cewXg9nJOiMERBbj0U24hPRsh9ed9b38vAwdpACmx0diL+Qyr7Q3MnMNuPC9PuvMoLLrzxGbQmaxLToaEbGsm9ah5vTSku6OYD3ek7qnfJpRY0/dMoDtwkBsF4ZU54Hql3ks9BScG8d1JmU6ExQjQhvzMn6CIlf/ivgVubNlTf9wKqtKqpxQrKMCRdcuPbWw57tvmUA/bKCcjEfRFwlAwY2tQ== root@t510-headless";
     t510.root = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9XruZXQNh1AigL32KmjeMwrKTwvP1T4Zc2SLGDUkq3 root@t510";
+
+    # Void laptop
+    void.tcarrio = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG/YX263nli8x0brqKtktwAaqmChPVyYcPqCKY5RDzZl tcarrio@t510";
+    void.host = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC9TxciSqBZXPjnHYlv1+2D1tjKYR2jLUigtANtdS34YESWveh0eiaYp7hHVi2BedVJOe/vKI6JllnX/CeNGtMI+PowVBniov5WvPkOTbS9p8fsGJP1OqPxlFR6jq7te6/KzLDbMMk91miFOTx27qZ2kWk01K7AGqNQ8BotBRojsAONB9M1JAv42CSuZmB/nKe2zWCpWoMhMGzqOD/TzrO3qLZ1rLUHkXV8cIWoDaWRngAV/sLMrrQglRvXz9GkNPq98JsXM0XL0GrEnEUz8rPx+kS7hF3Ysq2MNawd0CeaG8aeCoNwGep74nPT+V/nAs9xYa9M53fFAOO/Hoi0yxmZIe1324ure7QKgNLDiRIxPfIvf1LNvdNoxWjKKyaoTwRLh8E2Nn5dx94kzRfPaeMK6uMSNhoLu2blqI0omTmKi9OKgmmZpB1+2vK7C24HJQ2xQU4UR63SlwvTT+u38cewXg9nJOiMERBbj0U24hPRsh9ed9b38vAwdpACmx0diL+Qyr7Q3MnMNuPC9PuvMoLLrzxGbQmaxLToaEbGsm9ah5vTSku6OYD3ek7qnfJpRY0/dMoDtwkBsF4ZU54Hql3ks9BScG8d1JmU6ExQjQhvzMn6CIlf/ivgVubNlTf9wKqtKqpxQrKMCRdcuPbWw57tvmUA/bKCcjEfRFwlAwY2tQ== root@t510-headless";
 
     # NUC servers
     nuc0.host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHkgTzsmgHcVE12Sc9EYPP29Ek8d++RKZCIVEGEmWJc9 nuc0.int.carrio.dev";
@@ -25,24 +32,19 @@ rec {
     nuc7.host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHxoeRp7c4Nf8UNbeLn+2iNIF/aRYGB3Oguh/5B7tPeX nuc7.int.carrio.dev";
     nuc8.host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE3oKc5kg3bsi0+CeNveIAe72oBsUcpJneRtzHD19W5r nuc8.int.carrio.dev";
     nuc9.host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMTvNx3JktdcCnanqsDZiRJs2IeF9Gb8NVeLdBFHiekR nuc9.int.carrio.dev";
-
-    # Digital Ocean servers
-    nix-proxy-droplet.host = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCddQfWim3sfzyrOh7gg8bjhtpiKYWLQKff5yGxp3kQrykzVuOwKLWg182IBlk7VdIiIB/RDdwJ0umgpiTV+esFAtwokWS12CmVo5HFabGKmRYdr9fxgRXarMAHpsHabt7e5E96gwUyC0PuBfODTGc6Gm30SV6bHPS5faVv7lsUoW47sD4XPf2Nm7QJEjaZyXJjk88H37mTCAuNBlJ1+d/jkzSpkHGJ2Maaj5i8y0AEa3BQWQDG7xSgJvNeVeY9V/rMOmvSm9nUhIvMV2sqNQX6UVD13tMJfq3MtzTOOgEBrXk7PzUDHM5Tfea1ZZjkAW9zyuvS68vqX8Sv4+A+jzmuNvmZV2H4TOomJniorrsoLijST5pHNp53W1FB6asWQHp1NUr7Pk0twFQ0FpFbULhPDJfhvb0wDwEUT+yyBLeVMSe4EiRAIhWZBK2xVipSlQJzI8iAp/OilYCTJdS1EcCBwKJUQVDZYqTk1FP4QCf0jQCdc7znVe8c6nOxsR3lbTJFjvJwXEd0V3yQFUOOC9zQtFCuo5IA9EIZYMXkgmcQoxgoNwZh+KUmjGzS3Z2Txd0bw0VYIy1pHutk6AfjJMX5fV8bEtHxRm+xcwDc9oCCNCt7+QC65u5HMXrSkLz7t+fTW/3WeWuw6gBhwNAGuRdx++JfxpmMHHtuMcNt9Z3daw== root@nix-proxy-droplet";
   };
 
   # logical groups
-  groups = {
+  groups = rec {
     remote_build_clients = with systems; [
-      t510.tcarrio
-      t510.host
-      t510.root
     ];
     privileged_users = with systems; [
-      sktc0.tcarrio
-      glass.host
-      glass.tcarrio
-      t510.tcarrio
+      obsidian.host
+      obsidian.tcarrio
     ];
+    users = with systems; [
+      void.tcarrio
+    ] ++ privileged_users;
     deploy_keys = with systems; [
     ];
   };
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,37 +1,35 @@
 let
   sshMatrix = import ../lib/ssh-matrix.nix { };
-  inherit (sshMatrix) systems;
-  inherit (systems) glass obsidian t510;
+  inherit (sshMatrix) groups systems;
+  inherit (systems) glass obsidian sktc0 t510 void;
 
   autoMeshSystems = [
     glass.host
     systems.nix-proxy-droplet.host
   ];
-
-  roles = {
-    admin = [ glass.tcarrio obsidian.tcarrio t510.tcarrio ];
-    deploy = [ ];
-  };
 in
 {
-  "users/tcarrio/ssh.age".publicKeys = roles.admin ++ [ glass.tcarrio glass.host ];
-  "services/netbird/token.age".publicKeys = roles.admin ++ autoMeshSystems;
-  "services/tailscale/token.age".publicKeys = roles.admin ++ autoMeshSystems;
+  "users/tcarrio/ssh.age".publicKeys = groups.privileged_users ++ [ glass.tcarrio glass.host ];
+  "services/netbird/token.age".publicKeys = groups.privileged_users ++ autoMeshSystems;
+  "services/tailscale/token.age".publicKeys = groups.privileged_users ++ autoMeshSystems;
   # "services/jira-cli/token.age".publicKeys = macos;
-  "services/acme/cloudflare.age".publicKeys = roles.admin ++ [ systems.nix-proxy-droplet.host ];
-  "services/hoarder/env.age".publicKeys = roles.admin ++ [ systems.obsidian.host ];
+  "services/acme/cloudflare.age".publicKeys = groups.privileged_users ++ [ systems.nix-proxy-droplet.host ];
+  "services/hoarder/env.age".publicKeys = groups.privileged_users ++ [ systems.obsidian.host ];
 
-  "network-shares/ds418/smb.conf.age".publicKeys = roles.admin;
+  "network-shares/ds418/smb.conf.age".publicKeys = groups.privileged_users;
 
   # primarily maintained via agenix for convenience of scripting automations
-  "hosts/nuc0/ssh_host_ed25519_key.age".publicKeys = roles.admin;
-  "hosts/nuc1/ssh_host_ed25519_key.age".publicKeys = roles.admin;
-  "hosts/nuc2/ssh_host_ed25519_key.age".publicKeys = roles.admin;
-  "hosts/nuc3/ssh_host_ed25519_key.age".publicKeys = roles.admin;
-  "hosts/nuc4/ssh_host_ed25519_key.age".publicKeys = roles.admin;
-  "hosts/nuc5/ssh_host_ed25519_key.age".publicKeys = roles.admin;
-  "hosts/nuc6/ssh_host_ed25519_key.age".publicKeys = roles.admin;
-  "hosts/nuc7/ssh_host_ed25519_key.age".publicKeys = roles.admin;
-  "hosts/nuc8/ssh_host_ed25519_key.age".publicKeys = roles.admin;
-  "hosts/nuc9/ssh_host_ed25519_key.age".publicKeys = roles.admin;
+  "hosts/nuc0/ssh_host_ed25519_key.age".publicKeys = groups.privileged_users;
+  "hosts/nuc1/ssh_host_ed25519_key.age".publicKeys = groups.privileged_users;
+  "hosts/nuc2/ssh_host_ed25519_key.age".publicKeys = groups.privileged_users;
+  "hosts/nuc3/ssh_host_ed25519_key.age".publicKeys = groups.privileged_users;
+  "hosts/nuc4/ssh_host_ed25519_key.age".publicKeys = groups.privileged_users;
+  "hosts/nuc5/ssh_host_ed25519_key.age".publicKeys = groups.privileged_users;
+  "hosts/nuc6/ssh_host_ed25519_key.age".publicKeys = groups.privileged_users;
+  "hosts/nuc7/ssh_host_ed25519_key.age".publicKeys = groups.privileged_users;
+  "hosts/nuc8/ssh_host_ed25519_key.age".publicKeys = groups.privileged_users;
+  "hosts/nuc9/ssh_host_ed25519_key.age".publicKeys = groups.privileged_users;
+
+  # spotify / mopidy
+  "services/spotify/client.age".publicKeys = groups.privileged_users;
 }
