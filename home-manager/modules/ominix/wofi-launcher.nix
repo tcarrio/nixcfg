c8e8a72762e28e38e8d470c5f8b7a9010f787b7a
Tom Carrio <tom@carrio.dev>
2025-09-06 23:31:13 -0400

    fix: home-manager build issues with homeDirectory reference and incorrect path reference for mpris-proxy

--- home-manager/modules/ominix/wofi-launcher.nix
+++ home-manager/modules/ominix/wofi-launcher.nix
@@ -1,4 +1,6 @@
 { config, ... }: {
+  # The following lines allow `wofi` to detect applications installed by the system and home-manager configurations
+  # by generating symbolic links to the root directories of the respective component's application directories.
   home.file.".local/share/applications/nixos".source = config.lib.file.mkOutOfStoreSymlink "/run/current-system/sw/share/applications";
-  home.file.".local/share/applications/home-manager".source = config.lib.file.mkOutOfStoreSymlink "${config.home}/.nix-profile/share/applications/";
+  home.file.".local/share/applications/home-manager".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix-profile/share/applications/";
 }
