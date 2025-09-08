04fe6b461425f5bd89b160794f96ee59f6497af0
Tom Carrio <thomas.carrio@skillshare.com>
2025-09-08 14:34:51 -0400

    feat: auto-composer-auth command to fulfill GitHub auth from CLI

---
+++ pkgs/auto-composer-auth.nix
@@ -0,0 +1,30 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  ghCli = "${pkgs.gh}/bin/gh";
+in pkgs.writeShellScriptBin "auto-composer-auth" ''
+  #!${pkgs.bash}/bin/bash
+  set -e
+
+  echo "Checking if Composer GitHub token is set..."
+  if ! command -v composer &> /dev/null; then
+      echo "Composer is not installed. The utility requires a contextual composer to configure."
+      exit 1
+  fi
+
+  if ! composer config --global github-oauth.github.com 1>/dev/null 2>/dev/null; then
+    if ! ${ghCli} auth status 1>/dev/null 2>/dev/null; then
+      echo "Authenticating with GitHub"
+      ${ghCli} auth login
+    fi
+
+    if ! ${ghCli} auth status; then
+      echo "Failed to authenticate with GitHub. Cannot set the COMPOSER_AUTH environment variable."
+    else
+      gh_token="$(${ghCli} auth token)"
+      composer config --global github-oauth.github.com "$gh_token"
+    fi
+  else
+    echo "Composer GitHub token is already set!"
+  fi
+''
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,7 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # Build them using 'nix build .#example' or (legacy) 'nix-build -A example'
 { pkgs ? (import ../nixpkgs.nix) { }, mkBunDerivation, nixvim }: {
+  auto-composer-auth = pkgs.callPackage ./auto-composer-auth.nix {};
   awsesh = pkgs.callPackage ./awsesh.nix { inherit pkgs; };
   kube-rsync = pkgs.callPackage ./kube-rsync.nix { inherit pkgs; };
   mac-launcher = pkgs.callPackage ./mac-launcher.nix { inherit pkgs; };
