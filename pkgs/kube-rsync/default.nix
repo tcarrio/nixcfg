abf0a38e5f910877f3cd8a8dffec860c45dd9dec
Tom Carrio <thomas.carrio@skillshare.com>
2025-09-10 17:17:40 -0400

    feat: kube-rsync packaging with wrapping and dependencies patched and symlinked

---
+++ pkgs/kube-rsync/default.nix
@@ -0,0 +1,14 @@
+{ lib, pkgs, writeShellScriptBin, ... }:
+
+let
+  pname = "kube-rsync";
+  runtimePkgs = with pkgs; [ kubectl rsync ];
+  kubeRsyncPkg = (pkgs.writeScriptBin pname (builtins.readFile ./kube-rsync.sh)).overrideAttrs(old: {
+    buildCommand = "${old.buildCommand}\n patchShebangs $out";
+  });
+in pkgs.symlinkJoin {
+  name = pname;
+  paths = [ kubeRsyncPkg ] ++ runtimePkgs;
+  buildInputs = [ pkgs.makeWrapper ];
+  postBuild = "wrapProgram $out/bin/${pname} --prefix PATH : $out/bin";
+}
