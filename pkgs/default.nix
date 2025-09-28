7c7f27aafa4cea9f3c89e3e10def12614318386f
Tom Carrio <tom@carrio.dev>
2025-09-27 21:46:54 -0400

    fix: remove mac-launcher from pkg listing
    TODO: Fix the linux reference to macOS package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,10 +1,11 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # Build them using 'nix build .#example' or (legacy) 'nix-build -A example'
-{ pkgs ? (import ../nixpkgs.nix) { }, mkBunDerivation, nixvim }: {
+{ pkgs, mkBunDerivation, nixvim }: {
   awsesh = pkgs.callPackage ./awsesh.nix { inherit pkgs; };
   gh-composer-auth = pkgs.callPackage ./gh-composer-auth.nix {};
   kube-rsync = pkgs.callPackage ./kube-rsync/default.nix { inherit pkgs; };
-  mac-launcher = pkgs.callPackage ./mac-launcher.nix { inherit pkgs; };
+  # TODO: Fix non-Darwin build issue
+  # mac-launcher = pkgs.callPackage ./mac-launcher.nix { inherit pkgs; };
   zeit = pkgs.callPackage ./zeit.nix { };
   gqurl = pkgs.callPackage ./gqurl/default.nix {
     inherit mkBunDerivation;
