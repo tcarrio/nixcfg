0a42c42cd109b1f0910adeefb186ab12b39e2047
Tom Carrio <tom@carrio.dev>
2025-11-08 23:02:40 -0500

    feat: default mixin for vscode

---
+++ home-manager/mixins/desktop/vscode.nix
@@ -0,0 +1,30 @@
+{ config, ... }:
+let
+  userSettings = {
+    "workbench.iconTheme" = "vscode-icons";
+    "[jsonc]" = {
+        "editor.quickSuggestions" = {
+            "strings" = true;
+        };
+        "editor.suggest.insertMode" = "replace";
+    };
+    "[typescript]" = {
+        "editor.defaultFormatter" = "vscode.typescript-language-features";
+    };
+    "[json]" = {
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+    };
+    "editor.fontLigatures" = true;
+    "editor.fontSize" = 12;
+    "editor.fontFamily" = "'Ubuntu Mono derivative Powerline', 'CaskaydiaMono Nerd Font Propo', 'Droid Sans Mono', 'monospace', monospace";
+    "terminal.integrated.fontSize" = 12;
+    "terminal.integrated.fontFamily" = "'Ubuntu Mono derivative Powerline', 'CaskaydiaMono Nerd Font Propo', 'Droid Sans Mono', 'monospace', monospace";
+    "workbench.colorTheme" = "Terafox";
+  };
+in {
+  home = {
+    file = {
+      "${config.xdg.configHome}/Code/User/settings.json".text = builtins.toJSON userSettings;
+    };
+  };
+}
