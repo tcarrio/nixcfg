8e886ef114f19e5e05891da555789e137dc41c29
Tom Carrio <tom@carrio.dev>
2026-03-02 17:10:51 -0500

    feat: use gh cli contextual token for github mcp server

--- home-manager/mixins/console/ai/github-mcp.nix
+++ home-manager/mixins/console/ai/github-mcp.nix
@@ -1,20 +1,23 @@
 # NOTE: Example for nix-darwin / home-manager here:
 # https://github.com/ryantm/agenix/issues/50#issuecomment-1634714797
 
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 let
   githubMcpServer = pkgs.writeShellScriptBin "github-mcp-server" ''
-    GITHUB_PERSONAL_ACCESS_TOKEN="$(${pkgs.coreutils}/bin/cat ${config.age.secrets.github-mcp.path})" \
-      ${pkgs.github-mcp-server}/bin/github-mcp-server \
+    GITHUB_PERSONAL_ACCESS_TOKEN="$(gh auth token)"
+    if [ -z "$GITHUB_PERSONAL_ACCESS_TOKEN" ]; then
+      echo "Error: github-cli is not authenticated!" >&2
+      exit 1
+    fi
+
+    export GITHUB_PERSONAL_ACCESS_TOKEN
+
+    ${pkgs.github-mcp-server}/bin/github-mcp-server \
       --dynamic-toolsets \
       --read-only \
       "$@"
   '';
 in
 {
-  home.packages = [
-    githubMcpServer
-  ];
-
-  age.secrets.github-mcp.file = ../../../secrets/services/github-mcp/token.age;
+  home.packages = [githubMcpServer];
 }
