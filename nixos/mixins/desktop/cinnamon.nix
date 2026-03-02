03447913e6001c35ca75745df4d8189c4d22b77c
Tom Carrio <tom@carrio.dev>
2026-03-01 22:02:58 -0500

    refactor: use latest config location for cinnamon DE/DM

--- nixos/mixins/desktop/cinnamon.nix
+++ nixos/mixins/desktop/cinnamon.nix
@@ -8,13 +8,13 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.desktopManager.cinnamon.enable = true;
+  services.desktopManager.cinnamon.enable = true;
 
   # Use GDM for the display manager (login screen)
   services.displayManager.gdm.enable = true;
 
   # Disable Wayland (issues with Electron app rendering)
-  services.xserver.displayManager.gdm.wayland = false;
+  services.displayManager.gdm.wayland = false;
 
   # Enable udev rules
   services.udev.packages = with pkgs; [ gnome-settings-daemon ];
