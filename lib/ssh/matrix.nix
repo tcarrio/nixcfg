c191b2c5ed38f4810890410f8b0491a10b9fa680
Tom Carrio <tom@carrio.dev>
2026-03-16 08:45:24 -0400

    feat(yubikey): privileged FIDO key for system access

--- lib/ssh/matrix.nix
+++ lib/ssh/matrix.nix
@@ -2,6 +2,10 @@ rec {
   # exports the system definitions directly
   systems = import ./systems.nix;
 
+  manualKeys = {
+    yubikeyFido = "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBHd1Depf72rUBC6FCZ06ziB5SRik7FRfYaCDMoju2HkoWRT1X5655144waK0hUQ2ptgvqffDwX0YINiN2xZWFK0AAAAEc3NoOg== tom@carrio.dev";
+  };
+
   # collect logical groupings of systems
   groups = rec {
     remote_build_clients = with systems; [
@@ -10,6 +14,7 @@ rec {
     privileged_users = with systems; [
       obsidian.host
       obsidian.tcarrio
+      manualKeys.yubikeyFido
     ];
 
     users = with systems; [
