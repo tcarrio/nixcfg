e7db2824a1364c2dbbdb8dd146fb91dbacfa3824
Tom Carrio <tom@carrio.dev>
2026-03-16 11:28:54 -0400

    refactor: move unnecessary export of manualKeys from ssh matrix

--- lib/ssh/matrix.nix
+++ lib/ssh/matrix.nix
@@ -1,10 +1,10 @@
-rec {
-  # exports the system definitions directly
-  systems = import ./systems.nix;
-
+let
   manualKeys = {
     yubikeyFido = "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBHd1Depf72rUBC6FCZ06ziB5SRik7FRfYaCDMoju2HkoWRT1X5655144waK0hUQ2ptgvqffDwX0YINiN2xZWFK0AAAAEc3NoOg== tom@carrio.dev";
   };
+in rec {
+  # exports the system definitions directly
+  systems = import ./systems.nix;
 
   # collect logical groupings of systems
   groups = rec {
