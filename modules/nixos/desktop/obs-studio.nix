0656f7dceeae0a00fd0f4f93b48831a683ad0be8
Tom Carrio <tom@carrio.dev>
2026-03-19 00:11:14 -0400

    feat(obs): enhance the module with new options, hardware detection, and plugins

--- modules/nixos/desktop/obs-studio.nix
+++ modules/nixos/desktop/obs-studio.nix
@@ -1,34 +1,72 @@
 { lib, config, pkgs, ... }:
 let
+  inherit (lib) elem mkIf mkEnableOption mkOption optionals types;
+
   cfg = config.oxc.desktop.obs-studio;
+
+  isAmdHardware = elem "amd" config.services.xserver.videoDrivers;
+  isNvidiaHardware = elem "nvidia" config.services.xserver.videoDrivers;
+
+  package = if isNvidiaHardware
+    then (cfg.package.override {
+      cudaSupport = true;
+    })
+    else cfg.package;
+
+  basePlugins = with cfg.plugins; [
+    obs-3d-effect
+    obs-backgroundremoval
+    obs-command-source
+    obs-gradient-source
+    obs-gstreamer
+    obs-move-transition
+    obs-mute-filter
+    obs-pipewire-audio-capture
+    obs-scale-to-sound
+    obs-shaderfilter
+    obs-source-clone
+    obs-source-record
+    obs-source-switcher
+    obs-text-pthread
+    obs-vintage-filter
+    obs-vkcapture
+    obs-websocket
+    waveform
+    wlrobs
+  ];
+
+  amdPlugins = [pkgs.obs-studio-plugins.obs-vaapi];
+
+  plugins = basePlugins
+    ++ (optionals isAmdHardware amdPlugins);
 in
 {
   options.oxc.desktop.obs-studio = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Whether to enable the OBS Studio streaming software";
+    enable = mkEnableOption "Whether to enable the OBS Studio streaming software";
+    package = mkOption {
+      type = types.package;
+      default = pkgs.unstable.obs-studio;
+      description = "The package to use for OBS Studio";
     };
-
+    plugins = mkOption {
+      type = types.attrsOf types.package;
+      default = pkgs.unstable.obs-studio-plugins;
+      description = "The obs-studio-plugins group to source plugins";
+    };
     virtualCamera = {
-      enable = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
-        description = "Whether to enable the OBS Studio virtual camera";
-      };
+      enable = mkEnableOption "Whether to enable the OBS Studio virtual camera";
+    };
+    deviceCamera = {
+      # TODO: Utilize
+      droidCam = mkEnableOption "Whether to enable support for DroidCam";
+      # TODO: Utilize
+      dslr = mkEnableOption "Whether to enable support for DSLR cameras";
     };
 
-    # TODO: Breakdown additional components into separate options
-    bellsAndWhistles = {
-      all = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
-        description = "Whether to enable all the OBS Studio bells and whistles";
-      };
-    };
+    # TODO: Options to break up plugin installation into more modular sets
   };
 
-  config = lib.mkIf cfg.enable {
+  config = mkIf cfg.enable {
     # https://nixos.wiki/wiki/OBS_Studio
     boot =
       if cfg.virtualCamera.enable
@@ -39,35 +77,15 @@ in
         '';
       } else { };
 
+    programs.obs-studio = {
+      enable = true;
+      inherit package plugins;
+    };
+
     environment.systemPackages = [
       pkgs.bc
       pkgs.google-fonts
       pkgs.libnotify
-      (pkgs.wrapOBS {
-        plugins = with pkgs.obs-studio-plugins; if cfg.bellsAndWhistles.all then [
-          obs-3d-effect
-          obs-command-source
-          obs-gradient-source
-          obs-gstreamer
-          # TODO: Remove or add once broken build is fixed
-          # obs-nvfbc
-          obs-move-transition
-          obs-mute-filter
-          obs-pipewire-audio-capture
-          obs-text-pthread
-          obs-scale-to-sound
-          # advanced-scene-switcher
-          obs-shaderfilter
-          obs-source-clone
-          obs-source-record
-          obs-source-switcher
-          # obs-transition-table
-          obs-vaapi
-          obs-vintage-filter
-          obs-websocket
-          waveform
-        ] else [ ];
-      })
     ];
   };
 }
