b2102e42c72a1ff9cf81d6151beecfbd142fb805
Tom Carrio <tom@carrio.dev>
2026-03-20 11:12:59 -0400

    feat(zed): drop ai model and inline alternatives from zed config

--- modules/home-manager/zed.nix
+++ modules/home-manager/zed.nix
@@ -125,23 +125,9 @@ in
 
       userSettings = {
         agent = {
-          enabled = true;
+          enabled = false;
           default_open_ai_model = null;
-          ### PROVIDER OPTIONS
-          ### zed.dev models { claude-3-5-sonnet-latest } requires github connected
-          ### anthropic models { claude-3-5-sonnet-latest claude-3-haiku-latest claude-3-opus-latest  } requires API_KEY
-          ### copilot_chat models { gpt-4o gpt-4 gpt-3.5-turbo o1-preview } requires github connected
-          default_model = {
-            provider = "zed.dev";
-            model = "claude-3-5-sonnet-latest";
-          };
-
-          # inline_alternatives = [
-          #   {
-          #     provider = "copilot_chat";
-          #     model = "gpt-3.5-turbo";
-          #   }
-          # ];
+          default_model = null;
         };
 
         node.path = "${pkgs.nodejs}/bin/node";
