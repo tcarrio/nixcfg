376eadc5058fd304811c4dc518c1b0c62513480a
Tom Carrio <tom@carrio.dev>
2026-03-24 00:37:23 -0400

    fix(tailscale): use default value for mapping tailnet hosts to options

--- modules/nixos/services/tailnet.nix
+++ modules/nixos/services/tailnet.nix
@@ -1,3 +1,7 @@
-_: {
-  config.oxc.tailnet.hosts = (import ../../../lib/tailnet-matrix.nix);
+{ lib, ... }: {
+  options.oxc.tailnet.hosts = lib.mkOption {
+    type = lib.types.attrsOf lib.types.str;
+    default = import ../../../lib/tailnet-matrix.nix;
+    description = "Tailnet mapping of hostnames to IPv4 addresses";
+  };
 }
