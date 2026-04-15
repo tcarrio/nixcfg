6c4493a622a5fe565fae493435ab84d2aef2537f
Tom Carrio <tom@carrio.dev>
2026-04-15 18:17:05 -0400

    refactor: use pkgs.callPackage for all package definitions

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,33 +6,33 @@
   uv2nixLib,
   ...
 }:
+let
+
+in
 {
-  awsesh = pkgs.callPackage ./awsesh.nix { inherit pkgs; };
-  gh-composer-auth = pkgs.callPackage ./gh-composer-auth.nix { };
-  kube-rsync = pkgs.callPackage ./kube-rsync/default.nix { inherit pkgs; };
+  awsesh = pkgs.callPackage ./awsesh.nix {};
+  gh-composer-auth = pkgs.callPackage ./gh-composer-auth.nix {};
+  kube-rsync = pkgs.callPackage ./kube-rsync/default.nix {};
   # TODO: Fix non-Darwin build issue
   # mac-launcher = pkgs.callPackage ./mac-launcher.nix { inherit pkgs; };
-  zeit = pkgs.callPackage ./zeit.nix { };
-  pug = pkgs.callPackage ./pug.nix { };
+  zeit = pkgs.callPackage ./zeit.nix {};
+  pug = pkgs.callPackage ./pug.nix {};
   # TODO: Fix gqurl builder
   # gqurl = pkgs.callPackage ./gqurl/default.nix {
   #   inherit mkStandardBun;
   # };
   nixvim = pkgs.unstable.callPackage ./nixvim/default.nix {
     inherit nixvim;
-    pkgs = pkgs.unstable;
   };
-  serena = import ./serena/default.nix {
-    inherit (pkgs) lib;
-    inherit pkgs uv2nixLib;
+  serena = pkgs.callPackage ./serena/default.nix {
+    inherit uv2nixLib;
   };
-  sri-hash-gh-repo = import ./sri-hash-gh-repo.nix {
-    inherit pkgs;
-  };
-  uri-decode = import ./uri-decode.nix {
-    inherit pkgs;
-  };
-  qq-cli = pkgs.callPackage ./qq-cli.nix {
-    inherit pkgs;
-  };
+  ### TODO: Complete the custom buildPhase necessary for endcord
+  # endcord = pkgs.callPackage ./endcord.nix {
+  #   inherit (pkgs) lib;
+  #   inherit pkgs uv2nixLib;
+  # };
+  sri-hash-gh-repo = pkgs.callPackage ./sri-hash-gh-repo.nix {};
+  uri-decode = pkgs.callPackage ./uri-decode.nix {};
+  qq-cli = pkgs.callPackage ./qq-cli.nix {};
 }
