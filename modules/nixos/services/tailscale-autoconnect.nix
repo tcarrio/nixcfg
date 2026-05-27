fd416943a6060a7752b56867dd9f2a4355a7e95d
Tom Carrio <tom@carrio.dev>
2026-05-27 00:30:08 -0400

    refactor(tailscale): autoconnect service replaced with upstream NixOS service options for authKeyFile
    Wires the same strategy of agenix file mount but removes custom implementation

--- modules/nixos/services/tailscale-autoconnect.nix
+++ modules/nixos/services/tailscale-autoconnect.nix
@@ -1,6 +1,5 @@
 {
   config,
-  pkgs,
   lib,
   ...
 }:
@@ -25,43 +24,6 @@ in
       mode = "600";
     };
 
-    systemd.services.tailscale-autoconnect = {
-      description = "Automatic connection to Tailscale";
-
-      # make sure tailscale is running before trying to connect to tailscale
-      after = [
-        "network-pre.target"
-        "tailscale.service"
-      ];
-      wants = [
-        "network-pre.target"
-        "tailscale.service"
-        "run-agenix.d.mount"
-      ];
-      wantedBy = [ "multi-user.target" ];
-
-      # set this service as a oneshot job
-      serviceConfig.Type = "oneshot";
-
-      # have the job run this shell script
-      script = with pkgs; ''
-        # wait for tailscaled to settle
-        sleep 2
-
-        if [ ! -f "${config.age.secrets.tailscale-token.path}" ]; then
-          echo "Failed to find token secret at path '${config.age.secrets.tailscale-token.path}'"
-          exit 2
-        fi
-
-        # check if we are already authenticated to tailscale
-        status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-        if [ $status = "Running" ]; then # if so, then do nothing
-          exit 0
-        fi
-
-        # otherwise authenticate with tailscale
-        ${tailscale}/bin/tailscale up -authkey "$(cat "${config.age.secrets.tailscale-token.path}")"
-      '';
-    };
+    services.tailscale.authKeyFile = config.age.secrets.tailscale-token.path;
   };
 }
