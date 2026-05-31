59f978eb00203e49a891d8c4e8c38d08b8f03489
Tom Carrio <tom@carrio.dev>
2026-05-30 23:57:24 -0400

    fix(orca): manual networking configurations updated for new CIDR block

--- nixos/server/orca/default.nix
+++ nixos/server/orca/default.nix
@@ -21,10 +21,9 @@
 let
   inetConfig = {
     dns = [
-      "45.90.28.130"
-      "45.90.30.130"
+      "192.168.1.1"
     ];
-    gateway = "192.168.40.1";
+    gateway = "192.168.1.1";
   };
   externalHostnames = rec {
     base = "carrio.me";
@@ -124,12 +123,12 @@ in
   systemd.network.networks."20-lan" = {
     inherit (inetConfig) dns gateway;
     matchConfig.Name = "enp2s0";
-    address = "192.168.40.251/24";
+    address = "192.168.1.251/24";
   };
   systemd.network.networks."30-lan" = {
     inherit (inetConfig) dns gateway;
     matchConfig.Name = "enp3s0";
-    address = "192.168.40.250/24";
+    address = "192.168.1.250/24";
   };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
