62963318bfd161bdad6f9df1183454165bb51ff0
Tom Carrio <tom@carrio.dev>
2026-05-31 02:40:36 -0400

    fix(orca): additional required hass modules

--- nixos/server/orca/default.nix
+++ nixos/server/orca/default.nix
@@ -68,9 +68,14 @@ in
     enable = true;
     extraComponents = [
       # Components required to complete the onboarding
-      "esphome"
+      "analytics"
+      "google_translate"
       "met"
       "radio_browser"
+      "shopping_list"
+      # Recommended for fast zlib compression
+      # https://www.home-assistant.io/integrations/isal
+      "isal"
     ];
     config = {
       # Includes dependencies for a basic setup
