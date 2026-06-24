591f4642bfc069df9bb4cc15a223b8855ea03bcf
Tom Carrio <tom@carrio.dev>
2026-06-24 00:32:20 -0400

    fix: network shell fixes per platform type

--- shells/network/flake.nix
+++ shells/network/flake.nix
@@ -10,23 +10,33 @@
     {
       nixpkgs,
       flake-utils,
+      ...
     }:
 
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        isDarwin = pkgs.system == "x86_64-darwin" || pkgs.system == "aarch64-darwin";
       in
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            bmon # Modern Unix `iftop`
+            bind # dns-sd for mDNS/Bonjour
             curlie # Terminal HTTP client
-            dogdns # Modern Unix `dig`
+            dnsutils # dig, nslookup
+            gnugrep # Text processing
             httpie # Terminal HTTP client
             iperf3 # Terminal network benchmarking
             mtr # Modern Unix `traceroute`
+            net-tools # arp, ifconfig, netstat
+            nmap # Network scanning
+            tcpdump # Packet capture
+          ] ++ lib.optionals (isDarwin) [
             netdiscover # Modern Unix `arp`
+          ] ++ lib.optionals (!isDarwin) [
+            bmon # Modern Unix `iftop`
+            dogdns # Modern Unix `dig`
             nethogs # Modern Unix `iftop`
             ookla-speedtest # Terminal speedtest
             wavemon # Terminal WiFi monitor
